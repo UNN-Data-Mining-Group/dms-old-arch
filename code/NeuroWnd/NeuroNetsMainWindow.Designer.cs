c3b846327b2f3aa15c7fcf74a002ee9f2b21c7e1
Michael Smirnov <michael_smirnov@me.com>
2016-04-03 13:51:08 +0300

    Improved UI of neuro nets module

--- code/NeuroWnd/NeuroNetsMainWindow.Designer.cs
+++ code/NeuroWnd/NeuroNetsMainWindow.Designer.cs
@@ -79,23 +79,23 @@
             // 
             this.dgwNeuroNets.AllowUserToAddRows = false;
             this.dgwNeuroNets.AllowUserToDeleteRows = false;
-            this.dgwNeuroNets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgwNeuroNets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgwNeuroNets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgwNeuroNets.Location = new System.Drawing.Point(6, 126);
             this.dgwNeuroNets.Name = "dgwNeuroNets";
             this.dgwNeuroNets.ReadOnly = true;
             this.dgwNeuroNets.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Book Antiqua", 9.75F);
-            this.dgwNeuroNets.Size = new System.Drawing.Size(410, 257);
+            this.dgwNeuroNets.Size = new System.Drawing.Size(916, 232);
             this.dgwNeuroNets.TabIndex = 0;
             this.dgwNeuroNets.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dgwNeuroNets_MouseUp);
             // 
             // gbNeuroNets
             // 
-            this.gbNeuroNets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbNeuroNets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.gbNeuroNets.Controls.Add(this.lbRedactingInfo);
             this.gbNeuroNets.Controls.Add(this.gbParamsAF);
             this.gbNeuroNets.Controls.Add(this.btnDeleteNeuroNet);
@@ -106,31 +106,30 @@
             this.gbNeuroNets.Font = new System.Drawing.Font("Book Antiqua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.gbNeuroNets.Location = new System.Drawing.Point(6, 6);
             this.gbNeuroNets.Name = "gbNeuroNets";
-            this.gbNeuroNets.Size = new System.Drawing.Size(945, 392);
+            this.gbNeuroNets.Size = new System.Drawing.Size(931, 581);
             this.gbNeuroNets.TabIndex = 1;
             this.gbNeuroNets.TabStop = false;
             this.gbNeuroNets.Text = "База нейронных сетей";
             // 
             // lbRedactingInfo
             // 
-            this.lbRedactingInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbRedactingInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lbRedactingInfo.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lbRedactingInfo.Location = new System.Drawing.Point(6, 33);
             this.lbRedactingInfo.Name = "lbRedactingInfo";
-            this.lbRedactingInfo.Size = new System.Drawing.Size(930, 54);
+            this.lbRedactingInfo.Size = new System.Drawing.Size(916, 54);
             this.lbRedactingInfo.TabIndex = 4;
             this.lbRedactingInfo.Text = resources.GetString("lbRedactingInfo.Text");
             // 
             // gbParamsAF
             // 
-            this.gbParamsAF.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbParamsAF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.gbParamsAF.Controls.Add(this.dgwParamsAF);
             this.gbParamsAF.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.gbParamsAF.Location = new System.Drawing.Point(682, 105);
+            this.gbParamsAF.Location = new System.Drawing.Point(276, 364);
             this.gbParamsAF.Name = "gbParamsAF";
-            this.gbParamsAF.Size = new System.Drawing.Size(257, 281);
+            this.gbParamsAF.Size = new System.Drawing.Size(261, 211);
             this.gbParamsAF.TabIndex = 1;
             this.gbParamsAF.TabStop = false;
             this.gbParamsAF.Text = "Параметры активационной функции";
@@ -144,7 +143,7 @@
             this.dgwParamsAF.Location = new System.Drawing.Point(3, 20);
             this.dgwParamsAF.Name = "dgwParamsAF";
             this.dgwParamsAF.ReadOnly = true;
-            this.dgwParamsAF.Size = new System.Drawing.Size(251, 258);
+            this.dgwParamsAF.Size = new System.Drawing.Size(255, 188);
             this.dgwParamsAF.TabIndex = 0;
             // 
             // btnDeleteNeuroNet
@@ -173,13 +172,12 @@
             // 
             // gbNeuronsInLayers
             // 
-            this.gbNeuronsInLayers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbNeuronsInLayers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.gbNeuronsInLayers.Controls.Add(this.dgwNeuronsInLayers);
             this.gbNeuronsInLayers.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.gbNeuronsInLayers.Location = new System.Drawing.Point(419, 105);
+            this.gbNeuronsInLayers.Location = new System.Drawing.Point(6, 364);
             this.gbNeuronsInLayers.Name = "gbNeuronsInLayers";
-            this.gbNeuronsInLayers.Size = new System.Drawing.Size(260, 281);
+            this.gbNeuronsInLayers.Size = new System.Drawing.Size(264, 211);
             this.gbNeuronsInLayers.TabIndex = 0;
             this.gbNeuronsInLayers.TabStop = false;
             this.gbNeuronsInLayers.Text = "Распределение нейронов по слоям";
@@ -193,7 +191,7 @@
             this.dgwNeuronsInLayers.Location = new System.Drawing.Point(3, 20);
             this.dgwNeuronsInLayers.Name = "dgwNeuronsInLayers";
             this.dgwNeuronsInLayers.ReadOnly = true;
-            this.dgwNeuronsInLayers.Size = new System.Drawing.Size(254, 258);
+            this.dgwNeuronsInLayers.Size = new System.Drawing.Size(258, 188);
             this.dgwNeuronsInLayers.TabIndex = 0;
             // 
             // btnAddNeuroNet
@@ -209,16 +207,16 @@
             // 
             // tcInfoAboutNN
             // 
-            this.tcInfoAboutNN.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tcInfoAboutNN.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tcInfoAboutNN.Controls.Add(this.tpEditingNeuroNets);
             this.tcInfoAboutNN.Controls.Add(this.tpUsingNeuroNets);
             this.tcInfoAboutNN.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tcInfoAboutNN.Location = new System.Drawing.Point(7, 12);
             this.tcInfoAboutNN.Name = "tcInfoAboutNN";
             this.tcInfoAboutNN.SelectedIndex = 0;
-            this.tcInfoAboutNN.Size = new System.Drawing.Size(965, 437);
+            this.tcInfoAboutNN.Size = new System.Drawing.Size(951, 626);
             this.tcInfoAboutNN.TabIndex = 4;
             // 
             // tpEditingNeuroNets
@@ -227,7 +225,7 @@
             this.tpEditingNeuroNets.Location = new System.Drawing.Point(4, 29);
             this.tpEditingNeuroNets.Name = "tpEditingNeuroNets";
             this.tpEditingNeuroNets.Padding = new System.Windows.Forms.Padding(3);
-            this.tpEditingNeuroNets.Size = new System.Drawing.Size(957, 404);
+            this.tpEditingNeuroNets.Size = new System.Drawing.Size(943, 593);
             this.tpEditingNeuroNets.TabIndex = 0;
             this.tpEditingNeuroNets.Text = "Редактирование нейронных сетей";
             this.tpEditingNeuroNets.UseVisualStyleBackColor = true;
@@ -239,16 +237,16 @@
             this.tpUsingNeuroNets.Location = new System.Drawing.Point(4, 29);
             this.tpUsingNeuroNets.Name = "tpUsingNeuroNets";
             this.tpUsingNeuroNets.Padding = new System.Windows.Forms.Padding(3);
-            this.tpUsingNeuroNets.Size = new System.Drawing.Size(957, 404);
+            this.tpUsingNeuroNets.Size = new System.Drawing.Size(943, 593);
             this.tpUsingNeuroNets.TabIndex = 1;
             this.tpUsingNeuroNets.Text = "Использование нейронных сетей";
             this.tpUsingNeuroNets.UseVisualStyleBackColor = true;
             // 
             // gbUsingNeuroNets
             // 
-            this.gbUsingNeuroNets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbUsingNeuroNets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.gbUsingNeuroNets.Controls.Add(this.button1);
             this.gbUsingNeuroNets.Controls.Add(this.btnDelete);
             this.gbUsingNeuroNets.Controls.Add(this.lbLASelected);
@@ -267,7 +265,7 @@
             this.gbUsingNeuroNets.Font = new System.Drawing.Font("Book Antiqua", 18F);
             this.gbUsingNeuroNets.Location = new System.Drawing.Point(305, 6);
             this.gbUsingNeuroNets.Name = "gbUsingNeuroNets";
-            this.gbUsingNeuroNets.Size = new System.Drawing.Size(648, 388);
+            this.gbUsingNeuroNets.Size = new System.Drawing.Size(634, 577);
             this.gbUsingNeuroNets.TabIndex = 8;
             this.gbUsingNeuroNets.TabStop = false;
             this.gbUsingNeuroNets.Text = "Обученные/необученные нейронные сети";
@@ -288,7 +286,7 @@
             // 
             this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnDelete.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnDelete.Location = new System.Drawing.Point(556, 126);
+            this.btnDelete.Location = new System.Drawing.Point(542, 126);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(85, 35);
             this.btnDelete.TabIndex = 16;
@@ -380,24 +378,24 @@
             // 
             this.dgwLA.AllowUserToAddRows = false;
             this.dgwLA.AllowUserToDeleteRows = false;
-            this.dgwLA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgwLA.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgwLA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgwLA.Location = new System.Drawing.Point(368, 169);
+            this.dgwLA.Location = new System.Drawing.Point(9, 375);
             this.dgwLA.Name = "dgwLA";
             this.dgwLA.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Book Antiqua", 9.75F);
-            this.dgwLA.Size = new System.Drawing.Size(273, 212);
+            this.dgwLA.Size = new System.Drawing.Size(618, 195);
             this.dgwLA.TabIndex = 7;
             this.dgwLA.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dgwLA_MouseUp);
             // 
             // lbInfo
             // 
-            this.lbInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lbInfo.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lbInfo.Location = new System.Drawing.Point(6, 33);
             this.lbInfo.Name = "lbInfo";
-            this.lbInfo.Size = new System.Drawing.Size(636, 73);
+            this.lbInfo.Size = new System.Drawing.Size(622, 73);
             this.lbInfo.TabIndex = 5;
             this.lbInfo.Text = resources.GetString("lbInfo.Text");
             // 
@@ -405,7 +403,7 @@
             // 
             this.btnLearn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnLearn.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnLearn.Location = new System.Drawing.Point(368, 126);
+            this.btnLearn.Location = new System.Drawing.Point(354, 126);
             this.btnLearn.Name = "btnLearn";
             this.btnLearn.Size = new System.Drawing.Size(85, 35);
             this.btnLearn.TabIndex = 3;
@@ -417,7 +415,7 @@
             // 
             this.btnUse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnUse.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnUse.Location = new System.Drawing.Point(459, 126);
+            this.btnUse.Location = new System.Drawing.Point(445, 126);
             this.btnUse.Name = "btnUse";
             this.btnUse.Size = new System.Drawing.Size(91, 35);
             this.btnUse.TabIndex = 4;
@@ -429,26 +427,26 @@
             // 
             this.dgwNets.AllowUserToAddRows = false;
             this.dgwNets.AllowUserToDeleteRows = false;
-            this.dgwNets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgwNets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgwNets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgwNets.Location = new System.Drawing.Point(6, 169);
+            this.dgwNets.Location = new System.Drawing.Point(9, 169);
             this.dgwNets.Name = "dgwNets";
             this.dgwNets.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Book Antiqua", 9.75F);
-            this.dgwNets.Size = new System.Drawing.Size(356, 213);
+            this.dgwNets.Size = new System.Drawing.Size(618, 200);
             this.dgwNets.TabIndex = 2;
             this.dgwNets.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dgwNets_MouseUp);
             // 
             // gbTasksTree
             // 
-            this.gbTasksTree.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.gbTasksTree.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.gbTasksTree.Controls.Add(this.tvTaskSelections);
             this.gbTasksTree.Font = new System.Drawing.Font("Book Antiqua", 18F);
             this.gbTasksTree.Location = new System.Drawing.Point(6, 6);
             this.gbTasksTree.Name = "gbTasksTree";
-            this.gbTasksTree.Size = new System.Drawing.Size(293, 388);
+            this.gbTasksTree.Size = new System.Drawing.Size(293, 577);
             this.gbTasksTree.TabIndex = 7;
             this.gbTasksTree.TabStop = false;
             this.gbTasksTree.Text = "Дерево задач";
@@ -459,7 +457,7 @@
             this.tvTaskSelections.Font = new System.Drawing.Font("Book Antiqua", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tvTaskSelections.Location = new System.Drawing.Point(3, 33);
             this.tvTaskSelections.Name = "tvTaskSelections";
-            this.tvTaskSelections.Size = new System.Drawing.Size(287, 352);
+            this.tvTaskSelections.Size = new System.Drawing.Size(287, 541);
             this.tvTaskSelections.TabIndex = 5;
             this.tvTaskSelections.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvTaskSelections_AfterSelect);
             // 
@@ -467,9 +465,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.ClientSize = new System.Drawing.Size(970, 650);
             this.Controls.Add(this.tcInfoAboutNN);
-            this.MinimumSize = new System.Drawing.Size(986, 498);
+            this.MinimumSize = new System.Drawing.Size(986, 689);
             this.Name = "NeuroNetsMainWindow";
             this.ShowIcon = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
